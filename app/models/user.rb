7cc1dcc0d86bbc78a967ee48cf637c1acb88ff98
kamil <devkamilnaija@gmail.com>
2020-08-29 23:09:46 +0100

    ADDS CONTROLLER ACTIONS AND VIEWS TO USER

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
