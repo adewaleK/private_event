f93339626163c8cdb3dc097403a1bfd855043d36
kamil <devkamilnaija@gmail.com>
2020-09-04 23:40:12 +0100

    GENERATES EVENT MODEL WITH FIELDS

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      #log_in user
-      session[:user_id]=user.id
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger]='Invalid email/password combination'
       render'new'
@@ -15,9 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    #log_out
-    session.delete(:user_id)
-    @current_user=nil
+    log_out
     redirect_to login_path
   end
 end
