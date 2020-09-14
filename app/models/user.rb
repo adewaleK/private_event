1e4d769c1da5508705835c80a1d91f8002d9fcdc
kamil <devkamilnaija@gmail.com>
2020-09-14 02:30:00 +0100

    REMOVED LOGIC FROM VIEWS BY CREATING PARTIAL FILES

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,6 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
   has_many :attended_events, through: :guests, source: :event
 
-  # def past_events
-  #   @past_events = []
-  #   self.created_events.each do |event|
-  #     datetime = event.date
-  #     if datetime < Date.today
-  #       @past_events << event
-  #     end
-  #   end
-  #   return @past_events
-  # end
-
   def upcoming_events
     attended_events.where('date > date(\'now\')')
   end
