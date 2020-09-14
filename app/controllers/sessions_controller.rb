b722ee0ee93c7fc3764657e5a728cea3109dd865
kamil <devkamilnaija@gmail.com>
2020-09-14 16:11:36 +0100

    REMOVES A STICKLER ERROR

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+    auth = @user && @user.authenticate(params[:session][:password])
+    if auth
       log_in @user
       redirect_to @user
     else
