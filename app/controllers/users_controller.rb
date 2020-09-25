da2698f662290a1de1c598cd12238b76fa2cbffd
kamil <devkamilnaija@gmail.com>
2020-09-25 09:33:45 +0100

    REMOVES LOGIN RESTRICTION AFTER USER's SIGNUP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(users_params)
     if @user.save
-      redirect_to @user
+      redirect_to root_path
     else
       render 'new'
     end
